d481a8ebe7a732be1d9de044ece43a062ef0a623
GrantYjGe <ge42@wisc.edu>
2024-08-19 00:08:41 -0500

    user-service

---
+++ src/main/java/com/grant/userinfo/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.grant.userinfo.mapper;
+
+import com.grant.userinfo.dto.UserDTO;
+import com.grant.userinfo.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User mapUserDTOToUser(UserDTO userDTO);
+    UserDTO mapUserToUserDTO(User user);
+
+}
